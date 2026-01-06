62d4e30a126ff90d3b7d8c1326bfac96bd2b9834
shrikantsharma <shrikants0421@gmail.com>
2026-01-06 23:41:59 +0530

    Initial project setup

---
+++ app/src/main/java/com/example/assignemnt/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.assignemnt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.assignemnt.ui.theme.AssignemntTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AssignemntTheme {
+                Greeting("Android")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AssignemntTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/assignemnt/presentation/ListWidget.kt
@@ -0,0 +1,4 @@
+package com.example.assignemnt.presentation
+
+
+
